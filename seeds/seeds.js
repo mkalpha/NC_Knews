878cbee765b4970e6c36f1d98b79a09970310e6b
Andrew <andrewjworkman@googlemail.com>
2019-03-11 16:53:41 +0000

    created migration for comments table

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,12 +1,13 @@
 const { topicData, userData, articleData } = require('../db/data');
-
+const { createArticles } = require('../db/utils');
 
 exports.seed = function (knex, Promise) {
   return knex.migrate.rollback()
     .then(() => knex.migrate.latest())
     .then(() => knex.insert(topicData).into('topics').returning('*'))
-    .then(topicRows => knex.insert(userData).into('users').returning('*')
-      .then((topicRows, userRows) => { // topics rows isn't being passed down the chain of promises
-
-      }));
+    .then((topicRows) => knex.insert(userData).into('users').returning('*'))
+      .then((userRows) => { 
+        const articles = createArticles(articleData);
+        return knex.insert(articles).into('articles').returning('*');
+      });
 };
