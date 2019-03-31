b4d5ad15356139d26c2eb334b5613828809d17c1
Andrew <andrewjworkman@googlemail.com>
2019-03-31 11:50:03 +0100

    process env

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
 const knex = require('knex');
-const dbConfig = require('../knexfile');
+const dbConfig = process.env.NODE_ENV === 'production' ? process.env : require('../knexfile');
 
 module.exports = knex(dbConfig);
