1c3c22a4871128f93ec806935459ea658f8ee725
Andrew Workman <mkalpha@googlemail.com>
2025-02-04 12:44:39 +0000

    fix: env

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 COPY package.json /app
 RUN npm install
 COPY . /app
-EXPOSE 9090
+EXPOSE 8080
 CMD ["npm", "start"]
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 const ENV = process.env.NODE_ENV || 'development';
+console.log('ENV :', ENV);
 
 const baseConfig = {
   client: 'pg',
